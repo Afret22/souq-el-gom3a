afe3982c8f3f46d8fffd341df883d8455df2864e
Afret22 <bolak2500@gmail.com>
2020-12-02 18:43:04 +0200

    added category

--- backend/server.js
+++ backend/server.js
@@ -33,9 +33,11 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
+  
 } else {
   app.get("/", (req, res) => {
     res.send("Api is running");
